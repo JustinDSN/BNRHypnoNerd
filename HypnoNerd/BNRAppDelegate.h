54f904ba50e8408ea8718ad53d7761e371a207e9
Justin Steffen <JustinDSN@gmail.com>
2014-02-24 10:28:31 -0800

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Justin Steffen on 2/24/14.
+//  Copyright (c) 2014 Justin Steffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
