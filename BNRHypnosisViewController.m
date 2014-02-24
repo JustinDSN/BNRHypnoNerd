4f20aceec7119a805d6326524a6ef61be0a12566
Justin Steffen <JustinDSN@gmail.com>
2014-02-24 11:08:20 -0800

    Adding images Adding tab bar names and images

--- BNRHypnosisViewController.m
+++ BNRHypnosisViewController.m
@@ -11,6 +11,20 @@
 
 @implementation BNRHypnosisViewController
 
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        self.tabBarItem.title = @"Hypnotise";
+        
+        UIImage *i = [UIImage imageNamed:@"Hypno.png"];
+        
+        self.tabBarItem.image = i;
+    }
+    
+    return self;
+}
+
 - (void)loadView {
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] init];
     self.view = backgroundView;
